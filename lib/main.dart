f6cfb5c889bfa3fdf71329fada868e9e633ec44d
jaromi.istvan <jaromi.istvan@gmail.com>
2023-12-25 13:21:49 +0100

    Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -45,10 +46,19 @@ class Ball extends StatefulWidget {
 }
 
 class _BallState extends State<Ball> {
+  int ballNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Image.asset('images/ball1.png'),
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
+          //print('Pressed');
+        },
+        child: Image.asset('images/ball$ballNumber.png'),
+      ),
     );
   }
 }
